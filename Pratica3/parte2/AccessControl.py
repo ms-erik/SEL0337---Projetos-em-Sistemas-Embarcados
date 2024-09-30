3aead1d795047b17485f8ebf19d27e0bb309ca03
ms-erik <erikmelges@usp.br>
2024-09-30 15:48:41 -0300

    Fixes in Led behavior

--- Pratica3/parte2/AccessControl.py
+++ Pratica3/parte2/AccessControl.py
@@ -2,10 +2,24 @@ from mfrc522 import SimpleMFRC522
 from time import sleep
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(21, GPIO.OUT)
+GPIO.output(21, False)
+
 leitor = SimpleMFRC522()
 print("Aproxime a tag do leitor para leitura.")
-while True: 
-    id,texto = leitor.read()
-    print("ID: {}\nTexto: {}".format(id, texto))
-    sleep(3) 
+try:
+    while True: 
+        id,texto = leitor.read()
+        if:
+            print("Acesso permitido")
+            GPIO.output(21, True)
+        }
+        else:
+            print("Acesso negado");
+            GPIO.output(21, False)
+        sleep(3) 
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
